d52284829f64d22d941b03a2436c0879a80db624
Justin Smith <smithjustinn@hotmail.com>
2019-03-17 13:16:53 -0400

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,83 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.sessions.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.collections.LinkedHashSet
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Sessions) {
+    }
+
+    install(io.ktor.websocket.WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    routing {
+        val clients =
+            Collections.synchronizedSet(LinkedHashSet<ChatClient>())
+
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        webSocket("/myws/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+
+        webSocket("/chat") {
+            val client = ChatClient(this)
+            clients += client
+            try {
+                while (true) {
+                    val frame = incoming.receive()
+                    when (frame) {
+                        is Frame.Text -> {
+                            val text = frame.readText()
+                            // Iterate over all the connections
+                            val textToSend = "${client.name} said: $text"
+                            clients.toList().forEach { other ->
+                                other.session.outgoing.send(Frame.Text(textToSend))
+                            }
+                        }
+                    }
+                }
+            } finally {
+                clients -= client
+            }
+
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
+
+class ChatClient(val session: DefaultWebSocketSession) {
+    companion object {
+        var lastId = AtomicInteger(0)
+    }
+
+    val id = lastId.getAndIncrement()
+    val name = "user$id"
+}
